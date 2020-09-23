f3ab5c2b4397915923e3851a10dc9e93d2f3885a
Carlo Parata <carlo.parata@st.com>
2020-09-23 10:29:22 +0200

    Fix code spelling check

--- src/ComponentObject.h
+++ src/ComponentObject.h
@@ -56,7 +56,7 @@ class ComponentObject {
   public:
     /**
      * @brief     Initializing the component.
-     * @param[in] init pointer to device specific initalization structure.
+     * @param[in] init pointer to device specific initialization structure.
      * @retval    "0" in case of success, an error code otherwise.
      */
     virtual int Init() = 0;
